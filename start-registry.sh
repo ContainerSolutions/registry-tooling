3e9409f5aa0b97f74d413e74cd3b77ac4f56f9a2
Adrian Mouat <adrian.mouat@gmail.com>
2016-08-18 14:43:37 +0100

    Bit more user friendly.

--- start-registry.sh
+++ start-registry.sh
@@ -2,33 +2,51 @@
 set -e 
 set -o pipefail
 
-kubectl delete job create-certs copy-certs || true
+if [[ $(id -u) -ne 0 ]]; then
+  echo "This script expects to run as root e.g:"
+  echo "sudo $0"
+  exit 1
+fi
+
+echo "Deleting any old certs..."
+kubectl delete job create-certs copy-certs &> /dev/null || true 
+
+echo
+echo "Creating new certs..."
 kubectl create -f k8s/create-certs.yaml
 kubectl create -f k8s/copy-certs.yaml
-kubectl delete rc --namespace=kube-system kube-registry || true
+
+echo
+echo "Removing any old registry and starting new one..."
+kubectl delete rc --namespace=kube-system kube-registry &> /dev/null || true 
 kubectl create -f k8s/reg_controller.yaml
-kubectl delete svc --namespace=kube-system kube-registry || true
+kubectl delete svc --namespace=kube-system kube-registry &> /dev/null || true 
 kubectl create -f k8s/reg_service.yaml
 
 #wait for cert to become available
-echo -n "waiting for cert"
+echo
+echo -n "Waiting for cert to become available"
 set +e
-kubectl get --namespace=kube-system secret registry-cert 2>/dev/null
+kubectl get --namespace=kube-system secret registry-cert &> /dev/null
 rc=$?
 while [ $rc != 0 ]
 do
   sleep 1
   echo -n "."
-  kubectl get --namespace=kube-system secret registry-cert 2>/dev/null
+  kubectl get --namespace=kube-system secret registry-cert &>/dev/null
   rc=$?
 done
 set -e
 
-echo "\n"
-#write out registry cert to local machine
-kubectl get --namespace=kube-system secret registry-cert -o go-template --template '{{(index .data "ca.crt")}}' | base64 -d > /etc/docker/certs.d/kube-registry.kube-system.svc.cluster.local:31000/ca.crt
+echo 
+echo "Adding certificate to local machine..."
+kubectl get --namespace=kube-system secret registry-cert \
+  -o go-template --template '{{(index .data "ca.crt")}}' \
+  | base64 -d > \
+  /etc/docker/certs.d/kube-registry.kube-system.svc.cluster.local:31000/ca.crt
 
-echo "added cert"
+echo
+echo "Exposing registry via /etc/hosts"
 
 #make resolvable on local machine
 #this is very hacky 
@@ -37,7 +55,14 @@ K8S_MASTER=$(kubectl cluster-info | \
     sed -n 's/Kubernetes master is running at https:\/\/\([^:]*\):.*/\1/p')
 
 # sed would be a better choice than ed, but it wants to create a temp file :(
-printf 'g/kube-registry.kube-system.svc.cluster.local/d\nw\n' | ed /etc/hosts
+# turned off stderr here, as ed likes to write to it even in success case
+printf 'g/kube-registry.kube-system.svc.cluster.local/d\nw\n' \
+  | ed /etc/hosts 2> /dev/null
+
 echo "$K8S_MASTER kube-registry.kube-system.svc.cluster.local" >> /etc/hosts
 
-echo "configured /etc/hosts"
+echo
+echo "Set-up completed."
+echo
+echo "The registry should shortly be available at:"
+echo "kube-registry.kube-system.svc.cluster.local:31000"
